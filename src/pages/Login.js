e3e9677d1d10eba126c96f4b8a3d66a862f74987
luizdebem <luizdebem99@gmail.com>
2022-11-10 02:06:52 -0300

    Login and protected routes functionality.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,50 @@
-import React from "react"
+import React, { useState } from "react"
+import { Input, Button } from "@material-tailwind/react";
+import { AuthService } from "../services/AuthService";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function submit(e) {
+    e.preventDefault();
+    try {
+      const res = await AuthService.login({ email, password });
+      const { accessToken, user } = res.data;
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("user", JSON.stringify(user));
+      navigate("/");
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        const { error } = e.response.data;
+        alert(error);
+        return;
+      }
+      alert("Ocorreu um erro interno. Entre em contato com o suporte")
+      return;
+    }
+  }
+
   return (
-    <div>Login</div>
-  )
+    <main className="flex flex-col justify-center items-center h-screen">
+      <div className="p-5 bg-white rounded-xl">
+        <h1 className="text-center mb-5">Revisa Aí - Portal do Professor</h1>
+        <form className="flex flex-col" onSubmit={submit}>
+          <div className="mb-3">
+            <Input onChange={(e) => { setEmail(e.target.value) }} required size="lg" variant="outlined" label="Email" type="email" />
+          </div>
+          <div className="mb-3">
+            <Input onChange={(e) => { setPassword(e.target.value) }} required size="lg" variant="outlined" label="Senha" type="password" />
+          </div>
+          <Button size="lg" variant="outlined" className="mb-5" type="submit">Entrar</Button>
+        </form>
+        <label>Não possui uma conta? Cadastre-se clicando aqui.</label>
+      </div>
+    </main>
+  );
 }
 
 export default Login
---
+++ src/services/AuthService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export class AuthService {
+  static baseUrl = process.env.REACT_APP_API_URL + "/auth";
+
+  static login(data) {
+    return axios.post(this.baseUrl + "/login", data);
+  }
+
+  static refreshToken() {
+    return axios.post(this.baseUrl + "/refresh-token");
+  }
+}
