a2f9acca708793c143ce3d120ab6048a7e6b636a
luizdebem <luizdebem99@gmail.com>
2022-11-10 22:19:33 -0300

    Fixed set cookies issues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"
+import "./index.css";
+import { AuthService } from "./services/AuthService.js";
 
-ReactDOM.render(
+const minutesToMilisseconds = (minutes) => minutes * 1000 * 60;
+setInterval(() => {
+  AuthService.refreshToken().then((res) => {
+    localStorage.setItem("accessToken", res.data.accessToken);
+  });
+}, minutesToMilisseconds(1));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -4,10 +4,10 @@ export class AuthService {
   static baseUrl = process.env.REACT_APP_API_URL + "/auth";
 
   static login(data) {
-    return axios.post(this.baseUrl + "/login", data);
+    return axios.post(this.baseUrl + "/login", data, { withCredentials: true });
   }
 
   static refreshToken() {
-    return axios.post(this.baseUrl + "/refresh-token");
+    return axios.post(this.baseUrl + "/refresh-token", {}, { withCredentials: true });
   }
 }
