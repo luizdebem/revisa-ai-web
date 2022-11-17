88ede842918fa5fd215527b37643c8d1e65b9262
luizdebem <luizdebem99@gmail.com>
2022-11-17 00:54:50 -0300

    Show profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,10 +1,29 @@
-import React from "react"
+import { Button, Input } from "@material-tailwind/react"
+import React, { useEffect, useState } from "react"
 import BaseLayout from "../layouts/BaseLayout"
+import { UserService } from "../services/UserService";
 
 const MyProfile = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(UserService.user);
+  }, []);
+
   return (
     <BaseLayout>
-      <h1>Meu perfil</h1>
+      <form>
+        <div className="mb-4">
+          <Input readOnly onChange={(e) => { console.log(e.target.value) }} defaultValue={user.fullName} label="Nome completo" size="lg" variant="outline" type="text" />
+        </div>
+        <div className="mb-4">
+          <Input readOnly onChange={(e) => { console.log(e.target.value) }} defaultValue={user.email} label="E-mail" size="lg" variant="outline" type="email" />
+        </div>
+        <div className="mb-4">
+          <Input readOnly onChange={(e) => { console.log(e.target.value) }} label="Criar nova senha" size="lg" variant="outline" type="password" />
+        </div>
+        <Button className="mb-4 w-full" type="submit">Atualizar</Button>
+      </form>
     </BaseLayout>
   )
 }
